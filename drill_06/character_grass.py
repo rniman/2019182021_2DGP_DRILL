476ff866bccdb9dddd8870cc74a88806be401b8b
rniman <shinmg00@gmail.com>
2022-09-21 11:05:44 +0900

    lecture4 강의

---
+++ drill_06/character_grass.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+#grass.draw_now(400,30)
+#character.draw_now(400,90)
+
+x= 0
+while(x<800):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,90)
+    x = x+2
+    delay(0.01)
+
+#delay(5)
+
+close_canvas()
